fb0ef20212a408dcfd0b2df7e0d175c44513205f
LI JIAHAO <lijiahao99131@gmail.com>
2016-10-07 09:36:29 +0800

    Hello world

--- Helloworld-uwp/MainPage.xaml.cs
+++ Helloworld-uwp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,12 @@ namespace Helloworld_uwp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msg = new MessageDialog("Hello world", "很高兴见到你~");
+            await msg.ShowAsync();
         }
     }
 }
